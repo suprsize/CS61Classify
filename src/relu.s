84cd90f87c03b7b42f166bf657b89d3c965468b7
suprsize <hello@Hellos-MacBook-Pro.local>
2021-07-08 12:01:08 -0700

    finished almost relu with passing simple test

--- src/relu.s
+++ src/relu.s
@@ -15,24 +15,21 @@
 relu:
     # Prologue
 
-
+    addi t0, x0, 0
+    addi t1, x0, 0
 loop_start:
-    
-
-
-
-
-
-
-
+	bge t0, a1, loop_end
+	slli t2, t0, 2
+	add  t3, a0, t2
+    lw t4, 0(t3)
+    bge t4, zero, loop_continue
+	sw zero, 0(t3)
 loop_continue:
-
-
-
+	addi t0, t0, 1
+	jal x0 loop_start
 loop_end:
 
 
     # Epilogue
 
-    
 	ret
